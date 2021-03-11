3c3c72c6cb4dc1b6c49eb79a40eb3110ca43f384
jarbasal <jarbasai@mailfence.com>
2021-03-11 22:18:09 +0000

    better_cps

---
+++ README.md
@@ -0,0 +1,34 @@
+# <img src='./res/icon/dust_icon.png' card_color='#40DBB0' width='50' height='50' style='vertical-align:bottom'/>DUST Skill
+
+![](./res/dust_logo.png)
+
+## About 
+
+Dust short science fiction movies for Mycroft
+
+![](./gui.png)
+![](./gui2.png)
+
+## Installation Notes
+
+this requires [skill-better-playback-control](https://github.com/JarbasSkills/skill-better-playback-control), it will blacklist and replace the official mycroft skill
+
+## Examples 
+
+* "open dust"
+* "play sci fi short movie"
+* "play dust"
+
+## Credits 
+- JarbasAl
+- [AIIX ](https://github.com/AIIX/) - GUI
+- [Dust](https://www.youtube.com/c/watchdust)
+
+## Category
+**Entertainment**
+
+## Tags
+#scifi
+#shortfilms
+#video
+#entertainment
---
+++ __init__.py
@@ -0,0 +1,78 @@
+from ovos_utils.skills.templates.video_collection import VideoCollectionSkill
+from mycroft.skills.core import intent_file_handler
+from pyvod import Collection, Media
+from os.path import join, dirname, basename
+from ovos_utils.playback import CPSMatchType, CPSPlayback, CPSMatchConfidence
+
+
+class OmeletoSkill(VideoCollectionSkill):
+
+    def __init__(self):
+        super().__init__("Omeleto")
+        self.supported_media = [CPSMatchType.GENERIC,
+                                CPSMatchType.MOVIE,
+                                CPSMatchType.SHORT_FILM,
+                                CPSMatchType.VIDEO]
+        self.message_namespace = basename(dirname(__file__)) + ".jarbasskills"
+        # load video catalog
+        path = join(dirname(__file__), "res", "omeleto.jsondb")
+        logo = join(dirname(__file__), "res", "logo.png")
+        self.media_collection = Collection("omeleto", logo=logo, db_path=path)
+        self.default_image = join(dirname(__file__), "ui", "icon.png")
+        self.skill_logo = join(dirname(__file__), "ui", "icon.png")
+        self.skill_icon = join(dirname(__file__), "ui", "icon.png")
+        self.default_bg = logo
+        self.media_type = CPSMatchType.SHORT_FILM
+        self.playback_type = CPSPlayback.GUI
+
+    # voice interaction
+    def get_intro_message(self):
+        self.speak_dialog("intro")
+
+    @intent_file_handler('home.intent')
+    def handle_homescreen_utterance(self, message):
+        self.handle_homescreen(message)
+
+    # better common play
+    def normalize_title(self, title):
+        title = title.lower().strip()
+        title = self.remove_voc(title, "omeleto")
+        title = self.remove_voc(title, "movie")
+        title = self.remove_voc(title, "video")
+        title = self.remove_voc(title, "scifi")
+        title = self.remove_voc(title, "short")
+        title = self.remove_voc(title, "horror")
+        title = title.replace("|", "").replace('"', "") \
+            .replace(':', "").replace('”', "").replace('“', "") \
+            .strip().split("|")[-1]
+        return " ".join([w for w in title.split(" ") if w])  # remove extra spaces
+
+    def match_media_type(self, phrase, media_type):
+        score = 0
+        if self.voc_match(phrase, "video") or media_type == CPSMatchType.VIDEO:
+            score += 5
+
+        if self.voc_match(phrase, "short"):
+            score += 15
+
+        if self.voc_match(phrase, "scifi"):
+            score += 5
+
+        if self.voc_match(phrase, "horror"):
+            score += 5
+
+        if self.voc_match(phrase, "movie") or media_type == CPSMatchType.MOVIE:
+            score += 15
+
+        if self.voc_match(phrase, "omeleto"):
+            score += 60
+
+        if media_type == CPSMatchType.SHORT_FILM:
+            score += 25
+
+        return score
+
+
+def create_skill():
+    return OmeletoSkill()
+
