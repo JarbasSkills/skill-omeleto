7dcbdd16c7f69c6d8aa35069c1f9b32a213c2b0d
jarbasal <jarbasai@mailfence.com>
2021-08-14 14:02:15 +0100

    .

--- __init__.py
+++ __init__.py
@@ -1,30 +1,32 @@
 from mycroft.skills.core import intent_file_handler
 from pyvod import Collection, Media
 from os.path import join, dirname, basename
-from ovos_workshop.frameworks.playback import CPSMatchType, CPSPlayback, \
-    CPSMatchConfidence
+from ovos_workshop.frameworks.playback import CommonPlayMediaType, CommonPlayPlaybackType, \
+    CommonPlayMatchConfidence
 from ovos_workshop.skills.video_collection import VideoCollectionSkill
+import biblioteca
 
 
 class OmeletoSkill(VideoCollectionSkill):
 
     def __init__(self):
         super().__init__("Omeleto")
-        self.supported_media = [CPSMatchType.GENERIC,
-                                CPSMatchType.MOVIE,
-                                CPSMatchType.SHORT_FILM,
-                                CPSMatchType.VIDEO]
+        self.supported_media = [CommonPlayMediaType.GENERIC,
+                                CommonPlayMediaType.MOVIE,
+                                CommonPlayMediaType.SHORT_FILM,
+                                CommonPlayMediaType.VIDEO]
         self.message_namespace = basename(dirname(__file__)) + ".jarbasskills"
         # load video catalog
-        path = join(dirname(__file__), "res", "omeleto.jsondb")
+        base_folder = biblioteca.download("ytcat_omeleto")
+        path = join(base_folder, "omeleto.jsondb")
         logo = join(dirname(__file__), "res", "logo.png")
         self.media_collection = Collection("omeleto", logo=logo, db_path=path)
         self.default_image = join(dirname(__file__), "ui", "icon.png")
         self.skill_logo = join(dirname(__file__), "ui", "icon.png")
         self.skill_icon = join(dirname(__file__), "ui", "icon.png")
         self.default_bg = logo
-        self.media_type = CPSMatchType.SHORT_FILM
-        self.playback_type = CPSPlayback.GUI
+        self.media_type = CommonPlayMediaType.SHORT_FILM
+        self.playback_type = CommonPlayPlaybackType.GUI
 
     # voice interaction
     def get_intro_message(self):
@@ -50,7 +52,7 @@ class OmeletoSkill(VideoCollectionSkill):
 
     def match_media_type(self, phrase, media_type):
         score = 0
-        if self.voc_match(phrase, "video") or media_type == CPSMatchType.VIDEO:
+        if self.voc_match(phrase, "video") or media_type == CommonPlayMediaType.VIDEO:
             score += 5
 
         if self.voc_match(phrase, "short"):
@@ -62,13 +64,13 @@ class OmeletoSkill(VideoCollectionSkill):
         if self.voc_match(phrase, "horror"):
             score += 5
 
-        if self.voc_match(phrase, "movie") or media_type == CPSMatchType.MOVIE:
+        if self.voc_match(phrase, "movie") or media_type == CommonPlayMediaType.MOVIE:
             score += 15
 
         if self.voc_match(phrase, "omeleto"):
             score += 60
 
-        if media_type == CPSMatchType.SHORT_FILM:
+        if media_type == CommonPlayMediaType.SHORT_FILM:
             score += 25
 
         return score
