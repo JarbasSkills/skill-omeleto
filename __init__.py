0fc3a7b13accd773c2d87910e2884db477a4607b
jarbasai <jarbasai@mailfence.com>
2023-01-06 14:40:09 +0000

    scrap automation

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ from ovos_utils.parse import fuzzy_match
 from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
     ocp_search, ocp_featured_media
 from youtube_archivist import YoutubeMonitor
+import random
 
 
 class OmeletoSkill(OVOSCommonPlaybackSkill):
@@ -22,12 +23,14 @@ class OmeletoSkill(OVOSCommonPlaybackSkill):
         self.skill_icon = join(dirname(__file__), "ui", "icon.png")
 
     def initialize(self):
-        url = "https://www.youtube.com/c/Omeleto/videos"
         bootstrap = "https://github.com/JarbasSkills/skill-omeleto/raw/dev/bootstrap.json"
         self.archive.bootstrap_from_url(bootstrap)
-        self.archive.monitor(url)
-        self.archive.setDaemon(True)
-        self.archive.start()
+        self.schedule_event(self._sync_db, random.randint(3600, 24 * 3600))
+
+    def _sync_db(self):
+        url = "https://www.youtube.com/c/Omeleto/videos"
+        self.archive.parse_videos(url)
+        self.schedule_event(self._sync_db, random.randint(3600, 24*3600))
 
     # matching
     def normalize_title(self, title):
